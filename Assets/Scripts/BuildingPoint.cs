0440573e3d32899fc8043b59081bf68501467930
jungtaek kim <kgajungtaek@gmail.com>
2024-03-01 21:19:28 +0900

    feat : Physics Raycaster 를 이용한 포인터 이벤트
    메인카메라의 위치에서 Raycast를 진행하여 인터페이스가 있는 컴포넌트와 상호작용

--- Assets/Scripts/BuildingPoint.cs
+++ Assets/Scripts/BuildingPoint.cs
@@ -1,9 +1,32 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class BuildingPoint : MonoBehaviour
+    , IPointerClickHandler
+    , IPointerEnterHandler
+    , IPointerExitHandler
 {
+    [SerializeField] new Renderer renderer;
+    [SerializeField] Color normal;
+    [SerializeField] Color highLight;
+
     public void Build()
     {
         Debug.Log("Build");
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Build();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        renderer.material.color = highLight;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        renderer.material.color = normal;
     }
 }
